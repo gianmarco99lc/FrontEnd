b4798973a58dc88f17a735b440de9e5ae04f377b
gianmarco99lc <gianmarcoleonecamelio@gmail.com>
2024-01-12 22:23:10 -0400

    sentencias

--- src/pages/Sentencias.jsx
+++ src/pages/Sentencias.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
+import { CircularProgress } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
+import axios from "axios";
 
 const Sentencias = () => {
   const [sentencias, setSentencias] = useState([]);
